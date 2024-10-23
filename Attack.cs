6d77c9c859ff64dead693f2066aca46b7c008bd2
Mark Bennison <markbennison@users.noreply.github.com>
2024-10-23 18:57:40 +0100

    Add project files.

---
+++ Attack.cs
@@ -0,0 +1,40 @@
+﻿namespace PolymorphismExample
+{
+    internal class Attack
+    {
+        public string Name { get; set; }
+
+        public virtual void DetectAttack()
+        {
+            Console.WriteLine("Detecting attack...");
+        }
+    }
+
+
+    internal class DoSAttack : Attack
+    {
+        public DoSAttack()
+        {
+            Name = "Denial-of-Service Attack";
+        }
+
+        public override void DetectAttack()
+        {
+            Console.WriteLine($"Detecting {Name}: Looking for excessive network traffic...");
+        }
+    }
+
+    internal class MalwareAttack : Attack
+    {
+        public MalwareAttack()
+        {
+            Name = "Malware Infection";
+        }
+
+        public override void DetectAttack()
+        {
+            Console.WriteLine($"Detecting {Name}: Analyzing system logs for suspicious activity...");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PolymorphismExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunIDS();
+
+            Console.ReadLine();
+        }
+
+        static void RunIDS()
+        {
+            Attack[] attacks = new Attack[]
+            {
+            new DoSAttack(),
+            new MalwareAttack()
+            };
+
+            foreach (Attack attack in attacks)
+            {
+                attack.DetectAttack();
+            }
+        }
+    }
+}
+
+
